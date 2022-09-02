8219f1a1c8ec8635de5354fa06d6a46d685653b2
alexPouivet <pouivet.alexandre@gmail.com>
2022-09-02 11:44:47 +0200

    fix width of curve for all screens

--- assets/curves/curveGrey.js
+++ assets/curves/curveGrey.js
@@ -3,9 +3,9 @@ import Svg, { G, Path, Rect, Ellipse, Circle, Mask, Line, Text as TextSvg, TSpan
 
 export default function curve() {
   return (
-	<Svg width="390" height="24" viewBox="0 0 390 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<Mask id="mask0_16_69" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="390" height="24">
-	<Rect width="390" height="24" fill="#fff"/>
+	<Svg width="100%" height="24" viewBox="0 0 390 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<Mask id="mask0_16_69" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="100%" height="24">
+	<Rect width="100%" height="24" fill="#fff"/>
 	</Mask>
 	<G mask="url(#mask0_16_69)">
 	<Ellipse cx="194.5" cy="697.958" rx="757.5" ry="697.958" fill="#F3F3F3"/>
